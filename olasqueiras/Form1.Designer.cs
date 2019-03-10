2f1b2e0fa90b130e7ee1c8c31b955bd48fa1b63f
SrShadowy <43967231+SrShadowy@users.noreply.github.com>
2019-03-10 19:27:49 -0300

    up

--- olasqueiras/Form1.Designer.cs
+++ olasqueiras/Form1.Designer.cs
@@ -87,6 +87,7 @@
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Bla bla bla desafio?";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- olasqueiras/Form1.cs
+++ olasqueiras/Form1.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace olasqueiras
@@ -40,6 +32,11 @@ namespace olasqueiras
         {
             Form ceza = new cripooooocripo_o_super_cao();
             ceza.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
